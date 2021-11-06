58aa7e611f5e3637cddaa3d5942dbafee98b0b3c
Ellis John <ellisjohn@zipcodembpchuck.myfiosgateway.com>
2021-11-05 20:00:39 -0400

    lab

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington;
 
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
 import java.util.Arrays;
 
 /**
@@ -80,7 +82,7 @@ public class StringArrayUtils {
         String[] result = new String[array.length]; // with empty slots
 
         for (int i = 0; i < array.length; i++) {
-            result[result.length-1-i] = array[i];
+            result[result.length - 1 - i] = array[i];
 
         }
         return result;
@@ -91,11 +93,11 @@ public class StringArrayUtils {
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
     public static boolean isPalindromic(String[] array) {
-        String[] reversed = reverse(array);
+        String[] reversed = reverse(array);       // just using above method to reverse String[] array
         if (Arrays.equals(reversed, array)) {     // arrays.equals is an array util to compare arrays ( pass in arrays)
             return true;
         }
-       return false;
+        return false;
     }
 
     /**
@@ -104,7 +106,25 @@ public class StringArrayUtils {
      */ // TODO
     public static boolean isPangramic(String[] array) {
 
+        // needs to return a T/F statement
+
+        String newString = Arrays.toString(array);  // converted array to string
+
+        if (newString.length() < 26) {    // if this string is less than 26, has to be false
+            return false;
+        }
+
+        // created a new array with whole alphabet
+        String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+        for (int i = 0; i < array.length; i++) {   // for loop to iterate through array
+            if (!newString.contains(alphabet[i])) {    // if newString does not contain alphabet at i
+                return false;                          // return false
+            }
+        }
+        return true;                                // otherwise, return true
     }
+
 
     /**
      * @param array array of String objects
@@ -113,16 +133,22 @@ public class StringArrayUtils {
      */ // TODO
     public static int getNumberOfOccurrences(String[] array, String value) {
 
+        // declare variables we'll use, counter and i
+        Integer counter = 0;
+        int i;
+        // set up for loop to start at i, end at the end of array, and i increases by one each time
+        for (i = 0; i < array.length; i++) {
+            // set up if statement to compare if value equals the element each time
+            if (value.equals(array[i])) {
+                // increase counter by one if value and array[i] are the same
+                counter = counter + 1;
+            }
+
+        }
+        // return the final count
+        return counter;
 
 
-
-
-
-
-
-
-
-        return 0;
     }
 
     /**
@@ -130,8 +156,21 @@ public class StringArrayUtils {
      * @param valueToRemove value to remove from array
      * @return array with identical contents excluding values of `value`
      */ // TODO
-    public static String[] removeValue(String[] array, String valueToRemove) {
-        return null;
+    public static String[] removeValue(String[] array, String valueToRemove) {   ////// TODO please revisit
+
+
+        // convert String[] to a string
+        String newString = Arrays.toString(array);
+        // since both are now strings, we can .replace and replace valueToRemove with "" (that is, nothing)
+        // declared and assigned to new string stringWithValuesRemoved
+        String stringWithValueRemoved = newString.replace(valueToRemove, "");
+        // use .split method to split the elements on a space " ", to make array of each word
+        // declare and assign as a new variable arrayWithValueRemoved
+        String[] arrayWithValueRemoved = stringWithValueRemoved.split(" ");
+
+        return arrayWithValueRemoved;
+
+
     }
 
     /**
@@ -139,7 +178,19 @@ public class StringArrayUtils {
      * @return array of Strings with consecutive duplicates removes
      */ // TODO
     public static String[] removeConsecutiveDuplicates(String[] array) {
+        String[] removedDuplicatesArray = new String[5];
+        int arrayLength = array.length;
+        int counter = 0;
+
+        for (int i = 0; i < arrayLength; i++) {
+            if (array[i] == array[i + 1]) {
+                counter++;
+                array[counter] = array[i];
+            }
+        }
         return null;
+
+
     }
 
     /**
@@ -147,7 +198,20 @@ public class StringArrayUtils {
      * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
      */ // TODO
     public static String[] packConsecutiveDuplicates(String[] array) {
-        return null;
+        // string to hold the position at 1
+        String duplicates = array[0];
+
+        for (int i=1; i< array.length; i++) {
+            // if we are going through the array and we encounter a lettter thats is same to letter ebfore
+            if(array[i].equals(array[i-1])) {
+                duplicates = duplicates + array[i];
+            }
+            else {
+                duplicates = duplicates + " " + array[i];
+            }
+            System.out.println("duplicate " + duplicates);
+        }
+        return duplicates.split(" ");
     }
 
 
